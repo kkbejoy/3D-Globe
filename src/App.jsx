f9a430a4d5f1d3303eb3d98f3397e21c7e27b5cd
kkbejoy <kkbejoy@ymail.com>
2024-06-07 00:07:52 +0530

    earth basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useState, Suspense } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import Earth from "../public/Earth";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <div className="flex justify-center w-full h-screen">
+        <div className="h-[100vh] w-[100vh]">
+          <Canvas>
+            <ambientLight />
+            <OrbitControls />
+            <Suspense fallback={null}>
+              <Earth />
+            </Suspense>
+            <Environment preset="sunset" />
+          </Canvas>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
